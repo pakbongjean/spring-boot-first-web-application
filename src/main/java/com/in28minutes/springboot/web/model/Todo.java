105d09c076450c3a4f9b3e24dde424cc38f255a1
jean <surasak_cha@truecorp.co.th>
2019-12-11 09:42:21 +0700

    update

--- src/main/java/com/in28minutes/springboot/web/model/Todo.java
+++ src/main/java/com/in28minutes/springboot/web/model/Todo.java
@@ -5,19 +5,13 @@ import java.util.Date;
 import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
-
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
-@EqualsAndHashCode
 
 public class Todo {
 
